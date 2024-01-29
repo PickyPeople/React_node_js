a5badb30399e59e32b4e9416a89166821aa0d350
luke <jjyjjh33@gmail.com>
2024-01-29 16:18:24 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import { useEffect, useState} from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("h1 :)")
-    return () => console.log("bye :(");
-  },[]);
-//   useEffect(function(){
-//     console.log("h1 :)")
-//     return function(){
-//       console.log("bye :(");
-//     }
-//   }, []);
-//   return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) =>  !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (e) => { //input의 입력값을 나타내기 위해서 반드시 필요한 것이다.
+    setTodo(e.target.value);
+  }
+  const onSubmit = (e) => {
+    e.preventDefault(); //버튼을 누르고 새로고침이 되는 것을 막기 위하여
+    if(toDo === ""){
+      return; 
+    }
+    // setTodo(""); //검색을 누르고 난 다음 검색창을 지우기 위한 것
+    setTodos((curretArray) => [toDo, ...curretArray]); //toDos의 배열에 새로운 배열인수를 넣어주기 위한 식
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick} >{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDo} 
+          type="text" 
+          placeholder="write your to do" 
+        />
+        <button>Add TO Do</button>
+      </form>
+      
     </div>
   );
 }
