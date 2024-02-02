0bf9b7a74825023b8990c81526e0d8f03f43a244
luke <jjyjjh33@gmail.com>
2024-02-02 16:10:33 +0900

    second

--- src/App.js
+++ src/App.js
@@ -11,23 +11,27 @@ function App() {
     if(toDo === ""){
       return; 
     }
-    // setTodo(""); //검색을 누르고 난 다음 검색창을 지우기 위한 것
-    setTodos((curretArray) => [toDo, ...curretArray]); //toDos의 배열에 새로운 배열인수를 넣어주기 위한 식
+    setTodos((curretArray) => [toDo, ...curretArray]); //toDos의 배열에 새로운 배열인수를 넣어주기 위한 식 
+                                                       //currentArray는 인수가 아닌 함수이름이다. 함수의 기능이 => 의 뒤에 나와있는 형태
+    setTodo(""); //검색을 누르고 난 다음 검색창을 지우기 위한 것
   };
-  console.log(toDos);
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input 
-          onChange={onChange} 
-          value={toDo} 
+          onChange={onChange} //setTOdo에 새로 갱신되는 값을 toDo로 보내준다.
+          value={toDo}
           type="text" 
           placeholder="write your to do" 
         />
         <button>Add TO Do</button>
       </form>
-      
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>) } 
+        {/* {map이라는 함수는 각 argument마다 실행을 시켜주는 함수이다.} */}
+      </ul>
     </div>
   );
 }
